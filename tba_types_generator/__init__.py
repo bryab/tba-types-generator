15aa720c44bd40057248461ece12ee027631eb4d
Bryan Fordney <126733+bryab@users.noreply.github.com>
2025-03-06 17:20:22 -0500

    black format

--- tba_types_generator/__init__.py
+++ tba_types_generator/__init__.py
@@ -1,6 +1,8 @@
 import os
 import json5
 import typing
+from pathlib import Path
+
 from .typescript_builder import write_ts_from_class, write_ts_from_interface
 import subprocess
 from .parser.tba_parser import get_classes as get_core_classes
@@ -9,50 +11,68 @@ from .parser.tba_extended_parser import get_globals as get_extended_globals
 import logging
 
 logger = logging.getLogger(__name__)
-SKIP_CLASSES = ['QObject', 'BAPP_SpecialFolders', 'QScriptable', 'Labeled',
-                'QLineEdit', 'SCRIPT_QSWidget', 'QProcess', 'SCR_BaseInterface']
+SKIP_CLASSES = [
+    "QObject",
+    "BAPP_SpecialFolders",
+    "QScriptable",
+    "Labeled",
+    "QLineEdit",
+    "SCRIPT_QSWidget",
+    "QProcess",
+    "SCR_BaseInterface",
+]
+
+
+DEFAULT_OUTPUT_DIR = Path(__file__).parent.parent / "dist" / "tba-types"
+
+
+def _get_output_dir():
+    return Path(os.environ.get("TBA_TYPES_OUTPUT_DIR", DEFAULT_OUTPUT_DIR))
 
 
 def _load_extra_ts_files():
-    override_dir = './override'
+    override_dir = "./override"
     data = {}
     for filename in os.listdir(override_dir):
         root, ext = os.path.splitext(filename)
-        if ext == '.ts':
+        if ext == ".ts":
             filepath = os.path.join(override_dir, filename)
-            with open(filepath, 'r') as f:
+            with open(filepath, "r") as f:
                 data[root] = f.read()
     return data
 
 
 def prettify(filename):
     logger.info(f"Prettifying {filename}")
-    subprocess.call(['prettier', '--write', filename])
+    subprocess.call(["prettier", "--write", filename])
 
 
-def _generate_ts_from_data(host: str, version_num: str, all_classes: typing.Iterator[dict], all_globals: typing.Iterator[dict]):
+def _generate_ts_from_data(
+    host: str,
+    version_num: str,
+    all_classes: typing.Iterator[dict],
+    all_globals: typing.Iterator[dict],
+):
     extra_file_data = _load_extra_ts_files()
 
-    ts_dir = os.path.join(os.path.expanduser(
-        '~/dev/harmony/tba-types/'), host, str(version_num))
-    if not os.path.isdir(ts_dir):
-        os.makedirs(ts_dir)
-    ts_filename = os.path.join(ts_dir, 'index.d.ts')
+    ts_dir = _get_output_dir() / host / str(version_num)
+    ts_dir.mkdir(parents=True, exist_ok=True)
+    ts_filename = os.path.join(ts_dir, "index.d.ts")
     logger.info(f"Writing to {ts_filename}")
-    with open(ts_filename, 'w') as ts_file:
-        ts_file.write(extra_file_data['preamble'])
-        if host == 'harmony' and version_num >= 16:
+    with open(ts_filename, "w") as ts_file:
+        ts_file.write(extra_file_data["preamble"])
+        if host == "harmony" and version_num >= 16:
             logger.debug(f"Writing Harmony addons for >= 16")
-            ts_file.write('\n')
-            ts_file.write(extra_file_data['preamble_harmony16up'])
-        if host == 'storyboardpro':
+            ts_file.write("\n")
+            ts_file.write(extra_file_data["preamble_harmony16up"])
+        if host == "storyboardpro":
             logger.debug(f"Writing addons for Storyboard Pro")
-            ts_file.write('\n')
-            ts_file.write(extra_file_data['preamble_sbpro'])
-        ts_file.write('\n\n\n')
+            ts_file.write("\n")
+            ts_file.write(extra_file_data["preamble_sbpro"])
+        ts_file.write("\n\n\n")
 
         for class_data in all_classes:
-            if class_data['name'] in SKIP_CLASSES:
+            if class_data["name"] in SKIP_CLASSES:
                 continue
             logger.debug(f"Writing class: {class_data['name']}")
             write_ts_from_class(class_data, ts_file)
@@ -60,13 +80,13 @@ def _generate_ts_from_data(host: str, version_num: str, all_classes: typing.Iter
         for global_data in all_globals:
             write_ts_from_interface(global_data, ts_file)
 
-        ts_file.write('\n\n\n')
-        if host == 'harmony':
+        ts_file.write("\n\n\n")
+        if host == "harmony":
             logger.debug("Writing harmony-specific add-ons")
-            ts_file.write(extra_file_data['harmony_post'])
+            ts_file.write(extra_file_data["harmony_post"])
 
             if version_num >= 20:
-                ts_file.write(extra_file_data['preamble_harmony_extended'])
+                ts_file.write(extra_file_data["preamble_harmony_extended"])
 
     prettify(ts_filename)
 
@@ -86,38 +106,46 @@ def _generate_ts_from_data(host: str, version_num: str, all_classes: typing.Iter
 
 
 def get_all_classes_with_overrides(host, version_num):
-    with open('./override/override.jsonc', 'rb') as f:
+    with open("./override/override.jsonc", "rb") as f:
         override_data = json5.load(f)
 
     for data in get_all_classes(host, version_num):
-        if data['name'] in override_data['classes']:
-            override = override_data['classes'][data['name']]
+        if data["name"] in override_data["classes"]:
+            override = override_data["classes"][data["name"]]
             for key, val in override.items():
-                if key == 'add_slots':
+                if key == "add_slots":
                     for slot in val:
-                        data['slots'].append(slot)
+                        data["slots"].append(slot)
                 else:
                     data[key] = val
             logger.debug(f"Applying override: {override}")
 
-        for slot in data['slots']:
-            if slot['name'] in override_data['slots']:
-                slot_override = override_data['slots'][slot['name']]
+        for slot in data["slots"]:
+            if slot["name"] in override_data["slots"]:
+                slot_override = override_data["slots"][slot["name"]]
                 for key in slot_override:
-                    if key not in ('params'):
-                        logger.debug("Overriding slot: {0}".format(
-                            slot_override))
+                    if key not in ("params"):
+                        logger.debug("Overriding slot: {0}".format(slot_override))
                         slot[key] = slot_override[key]
-                if 'params' in slot_override:
-                    if slot_override.get('replace_params', False):
-                        slot['params'] = slot_override['params']
+                if "params" in slot_override:
+                    if slot_override.get("replace_params", False):
+                        slot["params"] = slot_override["params"]
                     else:
-                        for param in slot['params']:
+                        for param in slot["params"]:
                             param_override = next(
-                                (p for p in slot_override['params'] if p['name'] == param['name']), None)
+                                (
+                                    p
+                                    for p in slot_override["params"]
+                                    if p["name"] == param["name"]
+                                ),
+                                None,
+                            )
                             if param_override:
-                                logger.debug("Overriding param: {}:{}".format(
-                                    slot['name'], param_override))
+                                logger.debug(
+                                    "Overriding param: {}:{}".format(
+                                        slot["name"], param_override
+                                    )
+                                )
                                 param.update(param_override)
         yield data
 
@@ -137,7 +165,12 @@ def get_all_globals(host, version_num):
 def generate(host, version_num):
     logger.info(f"Generating Typescript for {host}:{version_num}")
     _generate_ts_from_data(
-        host, version_num, get_all_classes_with_overrides(host, version_num), get_all_globals(host, version_num))
+        host,
+        version_num,
+        get_all_classes_with_overrides(host, version_num),
+        get_all_globals(host, version_num),
+    )
+
 
 # def generate_from_json():
 #     versions = ["harmony15","harmony16","harmony17","harmony20","harmony21","harmony22","sbpro7","sbpro20","sbpro22"]
