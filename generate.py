22ebf519742a42aefa5d7ca2853a22085045038e
Bryan Fordney <126733+bryab@users.noreply.github.com>
2025-03-08 10:40:49 -0500

    add Harmony 24 to list

--- generate.py
+++ generate.py
@@ -4,7 +4,7 @@ import os
 from pathlib import Path
 from tba_types_generator import generate, DEFAULT_OUTPUT_DIR
 
-HARMONY_VERSIONS = [17, 20, 21, 22]
+HARMONY_VERSIONS = [17, 20, 21, 22, 24]
 SBPRO_VERSIONS = [7, 20, 22]
 
 
